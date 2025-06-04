01ffd3790bdd87d562535dd74049e7e4cdfd5c75
Sasha Gerrand <sgerrand@users.noreply.github.com>
2025-06-04 10:06:02 +0100

    Fix invalid reference to `deps/0`
    ```
        error: undefined variable "deps"
        │
     11 │       deps: deps,
        │             ^^^^
        │
        └─ mix.exs:11:13: Example.Mixfile.project/0
    
    ** (CompileError) mix.exs: cannot compile module Example.Mixfile (errors have been logged)
    ```

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Example.Mixfile do
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
+      deps: deps(),
       preferred_cli_env: [
         "coveralls": :test,
         "coveralls.html": :test,
